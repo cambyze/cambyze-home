df7dc25b8e91786fb5cb7d57abfdd18a6bc84770
cambyze <cambyze@gmail.com>
2018-08-15 17:31:03 +0200

    Change db tables names
    Signed-off-by: cambyze <cambyze@gmail.com>

--- src/main/java/model/ApplicationModel.java
+++ src/main/java/model/ApplicationModel.java
@@ -3,34 +3,34 @@ package model;
 import java.util.List;
 import org.hibernate.Session;
 import org.hibernate.Transaction;
-import entities.Applications;
+import entities.Application;
 
-public class ApplicationsModel extends AbstractModel<Applications> {
+public class ApplicationModel extends AbstractModel<Application> {
 
-  public ApplicationsModel() {
-    super(Applications.class);
+  public ApplicationModel() {
+    super(Application.class);
   }
 
-  public List<Applications> findByName(String keyword) {
-    List<Applications> applications = null;
+  public List<Application> findByName(String keyword) {
+    List<Application> applicationList = null;
     Session session = null;
     Transaction transaction = null;
     try {
       session = sessionFactory.openSession();
       transaction = session.beginTransaction();
       org.hibernate.query.Query query =
-          session.createQuery("select a from Applications a where a.name like :keyword");
+          session.createQuery("select a from Application a where a.name like :keyword");
       query.setParameter("keyword", "%" + keyword + "%");
-      applications = query.getResultList();
+      applicationList = query.getResultList();
       transaction.commit();
     } catch (Exception e) {
-      applications = null;
+      applicationList = null;
       if (transaction != null) {
         transaction.rollback();
       }
     } finally {
       session.close();
     }
-    return applications;
+    return applicationList;
   }
 }
---
+++ src/main/java/model/MenuModel.java
@@ -0,0 +1,10 @@
+package model;
+
+import entities.Menu;
+
+public class MenuModel extends AbstractModel<Menu> {
+
+  public MenuModel() {
+    super(Menu.class);
+  }
+}
