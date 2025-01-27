16b520140c74c08099c89e0d1a5b2bcb717ffc42
cambyze <cambyze@gmail.com>
2025-01-27 07:11:16 +0100

    2025 upgrade + misc corrections
    Signed-off-by: cambyze <cambyze@gmail.com>

--- src/main/java/mailtools/MailBodies.java
+++ src/main/java/mailtools/MailBodies.java
@@ -26,7 +26,7 @@ public class MailBodies {
     html += "Our support team will contact you as soon as possible.</p>";
     html += "<p style=\"font-size: 17px; text-align: justify; color: #00008c;\">";
     html +=
-        "© 2020 CAMBYZE<a style=\"text-decoration: none; margin-left: 10px; margin-right: 10px\" ";
+        "2025 CAMBYZE<a style=\"text-decoration: none; margin-left: 10px; margin-right: 10px\" ";
     html += "href=\"https://cambyze.com/termsofservice/\" target=\"_blank\">Terms of service</a>";
     html +=
         "<a style=\"text-decoration: none;\" href=\"mailto:support@cambyze.com\">Contact us</a></p>";
@@ -41,11 +41,11 @@ public class MailBodies {
     String html =
         "<html><body><div stype=\"height: 100%; margin: 0\"><section style=\"margin-top: 20px; margin-left: 50px; margin-right: 50px; "
             + "background-color: #8eb4e3; margin-bottom: 10px;\">" + '\n';
-    html +=
-        "<div style=\"display: flex; flex-direction: row;\"><a  href=\"https://cambyze.com/\">" + '\n';
+    html += "<div style=\"display: flex; flex-direction: row;\"><a  href=\"https://cambyze.com/\">"
+        + '\n';
     html += "<img style=\"width: 400px; height: 80px;\" title=\"Cambyze\" alt=\"Cambyze\"" + '\n';
-    html +=
-        "src=\"https://cambyze.com/resources/images/cambyze_icon.png\" /></a></div></section>" + '\n';
+    html += "src=\"https://cambyze.com/resources/images/cambyze_icon.png\" /></a></div></section>"
+        + '\n';
     html +=
         "<div style=\"flex: 2; display: flex; margin-bottom: 10px; flex-direction: row;\">" + '\n';
     html +=
@@ -56,20 +56,22 @@ public class MailBodies {
             + "align-items: left; text-decoration: none; color: #00008c;\">" + '\n';
     html += "<p><b>Account request</b></p>" + "<p style=\"color: #fff;\">.</p>" + '\n';
     html += "<p>Account to validate: <b>" + userName + "</b></p>";
+    html += "<p>=> Password: " + password + "</p><p>=> Email: <b>" + email
+        + "</p></div></div></div>" + '\n';
     html +=
-        "<p>=> Password: " + password + "</p><p>=> Email: <b>" + email + "</p></div></div></div>"
+        "<div style=\"display: flex; background-color: #8eb4e3; margin-left: 50px; margin-right: 50px; margin-bottom: 10px; border: 1px solid #8eb4e3;\">"
             + '\n';
-    html +=
-        "<div style=\"display: flex; background-color: #8eb4e3; margin-left: 50px; margin-right: 50px; margin-bottom: 10px; border: 1px solid #8eb4e3;\">" + '\n';
     html += "<div style=\"padding: 0; margin: 0; display: flex;\">" + '\n';
     html +=
-        "<div style=\"font-size: 18px; color: #fff; margin-left: 55px; margin-right: 10px;\">\u00A9 2020 CAMBYZE</div>" + '\n';
+        "<div style=\"font-size: 18px; color: #fff; margin-left: 55px; margin-right: 10px;\">\u00A9 2020 CAMBYZE</div>"
+            + '\n';
+    html += "<div style=\"font-size: 18px; color: #fff; margin-left: 10px; margin-right: 10px;\">"
+        + '\n';
     html +=
-        "<div style=\"font-size: 18px; color: #fff; margin-left: 10px; margin-right: 10px;\">" + '\n';
-    html +=
-        "<a style=\"color: #fff; text-decoration: none;\" href=\"https://cambyze.com/termsofservice/\" target=\"_blank\">Terms</a></div>" + '\n';
-    html +=
-        "<div style=\"font-size: 18px; color: #fff; margin-left: 10px; margin-right: 10px;\">" + '\n';
+        "<a style=\"color: #fff; text-decoration: none;\" href=\"https://cambyze.com/termsofservice/\" target=\"_blank\">Terms</a></div>"
+            + '\n';
+    html += "<div style=\"font-size: 18px; color: #fff; margin-left: 10px; margin-right: 10px;\">"
+        + '\n';
     html +=
         "<a style=\"color: #fff; text-decoration: none;\" href=\"mailto:support@cambyze.com\">Contact us</a></div></div></div></div></body></html>";
 
