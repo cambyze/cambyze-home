aef4e2be161522b5e9db8a79cb5a5c51d85de01e
Thierry Nestelhut <support@cambyze.com>
2025-05-30 15:22:50 +0200

    fix tomcat integration
    Signed-off-by: Thierry Nestelhut <support@cambyze.com>

--- backend/src/main/java/com/cambyze/home/HomeApplication.java
+++ backend/src/main/java/com/cambyze/home/HomeApplication.java
@@ -2,12 +2,13 @@ package com.cambyze.home;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.ServletWebServerFactoryAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { ServletWebServerFactoryAutoConfiguration.class })
 public class HomeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(HomeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(HomeApplication.class, args);
+    }
 
 }
